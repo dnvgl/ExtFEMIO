e55c25febc9665dcfbfaf8d55b4cfac60e96f1dc
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2015-10-27 10:04:43 +0000

    Added more header files to preload.
    git-svn-id: https://svnsrv.germanlloyd.org/svn/ExtFEMIO/trunk@133 389f8db8-dd28-11e4-b689-8b400cb78fb4

--- include/stdafx.h
+++ include/stdafx.h
@@ -6,3 +6,16 @@
 #pragma once
 
 // TODO: reference additional headers your program requires here
+
+#include <regex>
+
+#include "catch.hpp"
+#include "my_c++14.h"
+#include "bdf/cards.h"
+#include "bdf/cards_elements.h"
+#include "bdf/cards_properties.h"
+#include "bdf/errors.h"
+#include "bdf/file.h"
+#include "bdf/string.h"
+#include "bdf/type_bounds.h"
+#include "bdf/types.h"
