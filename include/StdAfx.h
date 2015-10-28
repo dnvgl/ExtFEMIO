c50cdceb2ef8e367a0ff4feeab579cc96aa2a2b2
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2015-10-28 13:29:21 +0000

    Summary: Fixed precompiled header includes.
    git-svn-id: https://svnsrv.germanlloyd.org/svn/ExtFEMIO/trunk@143 389f8db8-dd28-11e4-b689-8b400cb78fb4

--- include/StdAfx.h
+++ include/StdAfx.h
@@ -1,4 +1,4 @@
-// stdafx.h : include file for standard system include files,
+// StdAfx.h : include file for standard system include files,
 // or project specific include files that are used frequently,
 // but are changed infrequently
 
@@ -11,5 +11,18 @@
 #include <set>
 #include <map>
 #include <utility>
+
+#include <regex>
+
+#include "catch.hpp"
+#include "my_c++14.h"
+#include "bdf/cards.h"
+#include "bdf/cards_elements.h"
+#include "bdf/cards_properties.h"
+#include "bdf/errors.h"
+#include "bdf/file.h"
+#include "bdf/string.h"
+#include "bdf/type_bounds.h"
+#include "bdf/types.h"
 
 #endif
