917885b451ed34635de8e3445acab394c6102111
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2015-12-03 08:56:44 +0000

    Summary: Improved tests for `CBEAM` cards.
    git-svn-id: https://svnsrv.germanlloyd.org/svn/ExtFEMIO/trunk@230 389f8db8-dd28-11e4-b689-8b400cb78fb4

--- tests/test_bdf_cards_cbeam.cpp
+++ tests/test_bdf_cards_cbeam.cpp
@@ -44,11 +44,16 @@ using namespace ::dnvgl::extfem::bdf::cards;
 
 TEST_CASE("BDF CBEAM definitions. (Small Field Format)", "[bdf_cbeam]" ) {
 
-   ::std::deque<string> data;
+   ::std::deque<string> data, data_2;
    data.push_back(
       "CBEAM   7869    104010  76      153     0.0     66.5206 997.785  2.\n");
    data.push_back(
       "                        0.0     -22.617 -339.25 0.0     -22.617 \n");
+   data_2.push_back(
+      "CBEAM   36      103023  7       9       0.0     1000.00 0.0\n");
+   data_2.push_back(
+      "                        0.0     -240.00 0.0     0.0     -240.00 0.0\n");
+
    ::std::deque<string> lines;
    card::card_split(data, lines);
    cbeam probe(lines);
@@ -78,15 +83,50 @@ TEST_CASE("BDF CBEAM definitions. (Small Field Format)", "[bdf_cbeam]" ) {
       CHECK_FALSE(probe.SA);
       CHECK_FALSE(probe.SB);
    }
+   SECTION("OFFT default") {
+      ::std::deque<string> lines;
+      card::card_split(data_2, lines);
+      cbeam probe(lines);
+
+      // CBEAM   36      103023  7       9       0.0     1000.00 0.0
+      //                         0.0     -240.00 0.0     0.0     -240.00 0.0
+      CHECK((long)probe.EID == 36);
+      CHECK((long)probe.PID == 103023);
+      CHECK((long)probe.GA == 7);
+      CHECK((long)probe.GB == 9);
+      CHECK_FALSE((bool)probe.G0 == 13);
+      CHECK((double)probe.X1 == 0.);
+      CHECK((double)probe.X2 == 1000.);
+      CHECK((double)probe.X3 == 0.);
+      CHECK(probe.choose_dir_code == cbeam::has_DVEC);
+      CHECK((::std::string)probe.OFFT == "GGG");
+      CHECK_FALSE((bool)probe.BIT);
+      CHECK(probe.choose_offt_bit == cbeam::has_OFFT);
+      deque<int> p_ref;
+      CHECK(probe.PA == p_ref);
+      CHECK(probe.PB == p_ref);
+      CHECK((double)probe.W1A == 0.);
+      CHECK((double)probe.W2A == -240);
+      CHECK((double)probe.W3A == 0.);
+      CHECK((double)probe.W1B == 0.);
+      CHECK((double)probe.W2B == -240.);
+      CHECK((double)probe.W3B == 0.);
+      CHECK_FALSE((bool)probe.SA);
+      CHECK_FALSE((bool)probe.SB);
+   }
 }
 
 TEST_CASE("BDF CBEAM definitions. (Small Field Format), dircode",
           "[bdf_cbeam,dcode]" ) {
 
-   ::std::deque<string> data;
+   ::std::deque<string> data, data_2;
    data.push_back(
       "CBEAM   7869    104010  76      153      13                     GOO     \n");
    data.push_back(
+      "                        0.0     -22.617 -339.25 0.0     22.617 ");
+   data_2.push_back(
+      "CBEAM   7869    104010  76      153      13\n");
+   data_2.push_back(
       "                        0.0     -22.617 -339.25 0.0     22.617 ");
    ::std::deque<string> lines;
    card::card_split(data, lines);
@@ -119,15 +159,12 @@ TEST_CASE("BDF CBEAM definitions. (Small Field Format), dircode",
    }
 
    SECTION("OFFT default") {
-      ::std::deque<string> data;
-      data.push_back(
-      "CBEAM   7869    104010  76      153      13\n");
-      data.push_back(
-         "                        0.0     -22.617 -339.25 0.0     22.617 ");
       ::std::deque<string> lines;
-      card::card_split(data, lines);
+      card::card_split(data_2, lines);
       cbeam probe(lines);
 
+      // CBEAM   7869    104010  76      153      13
+      //                         0.0     -22.617 -339.25 0.0     22.617
       CHECK((long)probe.EID == 7869);
       CHECK((long)probe.PID == 104010);
       CHECK((long)probe.GA == 76);
