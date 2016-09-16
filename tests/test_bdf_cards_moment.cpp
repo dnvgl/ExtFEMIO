e5a4883fcea099e1115e87bbcda71d86f918128a
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2016-09-16 15:54:16 +0000

    Summary: Improved test
    git-svn-id: https://svnsrv.germanlloyd.org/svn/ExtFEMIO/trunk@457 389f8db8-dd28-11e4-b689-8b400cb78fb4

--- tests/test_bdf_cards_moment.cpp
+++ tests/test_bdf_cards_moment.cpp
@@ -101,14 +101,12 @@ TEST_CASE("BDF MOMENT types output.", "[bdf_moment,out]" ) {
       long SID(2), G(5), CID(6);
       double F(2.9), N1(0.), N2(1.9), N3(0.);
       moment probe;
-      probe(&SID, &G, &CID, &F, &N1, &N2, &N3);
-      test << probe;
+      test << *probe(&SID, &G, &CID, &F, &N1, &N2, &N3);
       SID++;
       G++;
       CID++;
       F += 4.;
-      probe(&SID, &G, &CID, &F, &N1, &N2, &N3);
-      test << probe;
+      test << *probe(&SID, &G, &CID, &F, &N1, &N2, &N3);
       CHECK(test.str() ==
             "MOMENT         2       5       62.900+00 0.00+001.900+00 0.00+00\n"
             "MOMENT         3       6       76.900+00 0.00+001.900+00 0.00+00\n");
