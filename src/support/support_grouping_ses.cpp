b0699eac43fcacc55d1477af54c9321fa7beec6b
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2016-12-20 16:03:13 +0000

    Summary: Providing dummy session file reader.
    git-svn-id: https://svnsrv.germanlloyd.org/svn/ExtFEMIO/trunk@515 389f8db8-dd28-11e4-b689-8b400cb78fb4

---
+++ src/support/support_grouping_ses.cpp
@@ -0,0 +1,31 @@
+/**
+   \file support_grouping_ses.cpp
+   \author Berthold Höllmann <berthold.hoellmann@dnvgl.com>
+   \copyright Copyright © 2016 by DNV GL SE
+   \brief Read FE grouping from Nastran session file;
+
+   Detailed description
+*/
+
+// ID:
+namespace {
+   char const cID_support_grouping_ses[]
+#ifdef __GNUC__
+   __attribute__ ((__unused__))
+#endif
+      = "@(#) $Id$";
+}
+
+#include "support/grouping.h"
+
+using namespace dnvgl::extfem::support::GroupInfo;
+
+Session::Session(std::istream const &inp) : grp_info() {}
+
+// Local Variables:
+// mode: c++
+// coding: utf-8
+// c-file-style: "dnvgl"
+// indent-tabs-mode: nil
+// compile-command: "make -C ../../cbuild -j8&&make -C ../../cbuild test"
+// End:
