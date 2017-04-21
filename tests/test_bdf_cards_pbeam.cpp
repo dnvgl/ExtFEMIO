151df2b25a72f2e0bce139928bcabf7272f73863
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2017-04-21 12:37:09 +0000

    Summary: Splitted pbeam tests.
    git-svn-id: https://svnsrv.germanlloyd.org/svn/ExtFEMIO/trunk@641 389f8db8-dd28-11e4-b689-8b400cb78fb4

--- tests/test_bdf_cards_pbeam.cpp
+++ tests/test_bdf_cards_pbeam.cpp
@@ -36,9 +36,9 @@ using namespace std;
 using namespace dnvgl::extfem::bdf;
 using namespace dnvgl::extfem::bdf::cards;
 
-TEST_CASE("BDF PBEAM definitions.", "[bdf_PBEAM]") {
+TEST_CASE("BDF PBEAM definitions (Small Field Format).", "[bdf_pbeam]") {
 
-    SECTION("Small Field Format") {
+    SECTION("check value") {
 
         std::list<std::string> data({
                 //       PID     MID     A       I1      I2      I12     J
@@ -50,16 +50,19 @@ TEST_CASE("BDF PBEAM definitions.", "[bdf_PBEAM]") {
 
         CAPTURE( data.front() );
 
-        REQUIRE((long)probe.PID == 4000001);
-        REQUIRE((long)probe.MID == 3);
-        REQUIRE(probe.A == std::vector<double>({10460.}));
-        REQUIRE(probe.I1 == std::vector<double>({93690000.}));
-        REQUIRE(probe.I2 == std::vector<double>({1694000.}));
-        REQUIRE(probe.I12 == std::vector<double>({6.856e6}));
-        REQUIRE(probe.J == std::vector<double>({1.316e6}));
+        CHECK((long)probe.PID == 4000001);
+        CHECK((long)probe.MID == 3);
+        CHECK(probe.A == std::vector<double>({10460.}));
+        CHECK(probe.I1 == std::vector<double>({93690000.}));
+        CHECK(probe.I2 == std::vector<double>({1694000.}));
+        CHECK(probe.I12 == std::vector<double>({6.856e6}));
+        CHECK(probe.J == std::vector<double>({1.316e6}));
     }
+}
 
-    SECTION("Small Field Format2") {
+TEST_CASE("BDF PBEAM definitions (Small Field Format 2).", "[bdf_pbeam]") {
+
+    SECTION("check value") {
 
         std::list<std::string> data({
                 //       PID     MID     A       I1      I2      I12     J
@@ -71,16 +74,19 @@ TEST_CASE("BDF PBEAM definitions.", "[bdf_PBEAM]") {
 
         CAPTURE( data.front() );
 
-        REQUIRE((long)probe.PID == 4000001);
-        REQUIRE((long)probe.MID == 3);
-        REQUIRE(probe.A == std::vector<double>({10460.}));
-        REQUIRE(probe.I1 == std::vector<double>({93690000.}));
-        REQUIRE(probe.I2 == std::vector<double>({1694000.}));
-        REQUIRE(probe.I12 == std::vector<double>({6.856e6}));
-        REQUIRE(probe.J == std::vector<double>({1.316e6}));
+        CHECK((long)probe.PID == 4000001);
+        CHECK((long)probe.MID == 3);
+        CHECK(probe.A == std::vector<double>({10460.}));
+        CHECK(probe.I1 == std::vector<double>({93690000.}));
+        CHECK(probe.I2 == std::vector<double>({1694000.}));
+        CHECK(probe.I12 == std::vector<double>({6.856e6}));
+        CHECK(probe.J == std::vector<double>({1.316e6}));
     }
+}
 
-    SECTION("Free Field Format 1") {
+TEST_CASE("BDF PBEAM definitions (Free Field Format 1).", "[bdf_pbeam]") {
+
+    SECTION("check value") {
 
         std::list<std::string> data({
                 "PBEAM,1,2,3.,4.,5.,6.,7.,8.,9.,10.,11.,12.,13.,14.,15.,16.\n"});
@@ -90,25 +96,28 @@ TEST_CASE("BDF PBEAM definitions.", "[bdf_PBEAM]") {
 
         CAPTURE( data.front() );
 
-        REQUIRE((long)probe.PID == 1);
-        REQUIRE((long)probe.MID == 2);
-        REQUIRE(probe.A == std::vector<double>({3.}));
-        REQUIRE(probe.I1 == std::vector<double>({4.}));
-        REQUIRE(probe.I2 == std::vector<double>({5.}));
-        REQUIRE(probe.I12 == std::vector<double>({6.}));
-        REQUIRE(probe.J == std::vector<double>({7.}));
-        REQUIRE(probe.NSM == std::vector<double>({8.}));
-        REQUIRE(probe.C1 == std::vector<double>({9.}));
-        REQUIRE(probe.C2 == std::vector<double>({10.}));
-        REQUIRE(probe.D1 == std::vector<double>({11.}));
-        REQUIRE(probe.D2 == std::vector<double>({12.}));
-        REQUIRE(probe.E1 == std::vector<double>({13.}));
-        REQUIRE(probe.E2 == std::vector<double>({14.}));
-        REQUIRE(probe.F1 == std::vector<double>({15.}));
-        REQUIRE(probe.F2 == std::vector<double>({16.}));
+        CHECK((long)probe.PID == 1);
+        CHECK((long)probe.MID == 2);
+        CHECK(probe.A == std::vector<double>({3.}));
+        CHECK(probe.I1 == std::vector<double>({4.}));
+        CHECK(probe.I2 == std::vector<double>({5.}));
+        CHECK(probe.I12 == std::vector<double>({6.}));
+        CHECK(probe.J == std::vector<double>({7.}));
+        CHECK(probe.NSM == std::vector<double>({8.}));
+        CHECK(probe.C1 == std::vector<double>({9.}));
+        CHECK(probe.C2 == std::vector<double>({10.}));
+        CHECK(probe.D1 == std::vector<double>({11.}));
+        CHECK(probe.D2 == std::vector<double>({12.}));
+        CHECK(probe.E1 == std::vector<double>({13.}));
+        CHECK(probe.E2 == std::vector<double>({14.}));
+        CHECK(probe.F1 == std::vector<double>({15.}));
+        CHECK(probe.F2 == std::vector<double>({16.}));
     }
+}
 
-    SECTION("Free Field Format 2") {
+TEST_CASE("BDF PBEAM definitions (Free Field Format 2).", "[bdf_pbeam]") {
+
+    SECTION("check values") {
 
         std::list<std::string> data({
                 "PBEAM,1,2,3.,4.,5.,6.,7.,8.,9.,10.,11.,12.,13.,14.,15.,16.,\n",
@@ -118,53 +127,55 @@ TEST_CASE("BDF PBEAM definitions.", "[bdf_PBEAM]") {
         std::list<std::string> lines;
         __base::card::card_split(data, lines);
 
-        REQUIRE(lines.size() == 65);
+        CHECK(lines.size() == 65);
         pbeam probe(lines);
 
         CAPTURE( data.front() );
 
-        REQUIRE((long)probe.PID == 1);
-        REQUIRE((long)probe.MID == 2);
+        CHECK((long)probe.PID == 1);
+        CHECK((long)probe.MID == 2);
         std::vector<std::string> ref({"YES", "NO"});
         for (auto p : probe.SO) {
-            REQUIRE(p == ref.front());
+            CHECK(p == ref.front());
             ref.erase(ref.begin());
         }
-        REQUIRE(probe.X_XB == std::vector<double>({18., 34.}));
-        REQUIRE(probe.A == std::vector<double>({3., 19., 35.}));
-        REQUIRE(probe.I1 == std::vector<double>({4., 20., 36.}));
-        REQUIRE(probe.I2 == std::vector<double>({5., 21., 37.}));
-        REQUIRE(probe.I12 == std::vector<double>({6., 22., 38.}));
-        REQUIRE(probe.J == std::vector<double>({7., 23., 39.}));
-        REQUIRE(probe.NSM == std::vector<double>({8., 24., 40.}));
-        REQUIRE(probe.C1 == std::vector<double>({9., 25., 41.}));
-        REQUIRE(probe.C2 == std::vector<double>({10., 26., 42.}));
-        REQUIRE(probe.D1 == std::vector<double>({11., 27., 43.}));
-        REQUIRE(probe.D2 == std::vector<double>({12., 28., 44.}));
-        REQUIRE(probe.E1 == std::vector<double>({13., 29., 45.}));
-        REQUIRE(probe.E2 == std::vector<double>({14., 30., 46.}));
-        REQUIRE(probe.F1 == std::vector<double>({15., 31., 47.}));
-        REQUIRE(probe.F2 == std::vector<double>({16., 32., 48.}));
-
-        REQUIRE((double)probe.K1 == 49);
-        REQUIRE((double)probe.K2 == 50);
-        REQUIRE((double)probe.S1 == 51);
-        REQUIRE((double)probe.S2 == 52);
-        REQUIRE((double)probe.NSI_A == 53);
-        REQUIRE((double)probe.NSI_B == 54);
-        REQUIRE((double)probe.CW_A == 55);
-        REQUIRE((double)probe.CW_B == 56);
-        REQUIRE((double)probe.M1_A == 57);
-        REQUIRE((double)probe.M2_A == 58);
-        REQUIRE((double)probe.M1_B == 59);
-        REQUIRE((double)probe.M2_B == 60);
-        REQUIRE((double)probe.N1_A == 61);
-        REQUIRE((double)probe.N2_A == 62);
-        REQUIRE((double)probe.N1_B == 63);
-        REQUIRE((double)probe.N2_B == 64);
+        CHECK(probe.X_XB == std::vector<double>({18., 34.}));
+        CHECK(probe.A == std::vector<double>({3., 19., 35.}));
+        CHECK(probe.I1 == std::vector<double>({4., 20., 36.}));
+        CHECK(probe.I2 == std::vector<double>({5., 21., 37.}));
+        CHECK(probe.I12 == std::vector<double>({6., 22., 38.}));
+        CHECK(probe.J == std::vector<double>({7., 23., 39.}));
+        CHECK(probe.NSM == std::vector<double>({8., 24., 40.}));
+        CHECK(probe.C1 == std::vector<double>({9., 25., 41.}));
+        CHECK(probe.C2 == std::vector<double>({10., 26., 42.}));
+        CHECK(probe.D1 == std::vector<double>({11., 27., 43.}));
+        CHECK(probe.D2 == std::vector<double>({12., 28., 44.}));
+        CHECK(probe.E1 == std::vector<double>({13., 29., 45.}));
+        CHECK(probe.E2 == std::vector<double>({14., 30., 46.}));
+        CHECK(probe.F1 == std::vector<double>({15., 31., 47.}));
+        CHECK(probe.F2 == std::vector<double>({16., 32., 48.}));
+        CHECK((double)probe.K1 == 49);
+        CHECK((double)probe.K2 == 50);
+        CHECK((double)probe.S1 == 51);
+        CHECK((double)probe.S2 == 52);
+        CHECK((double)probe.NSI_A == 53);
+        CHECK((double)probe.NSI_B == 54);
+        CHECK((double)probe.CW_A == 55);
+        CHECK((double)probe.CW_B == 56);
+        CHECK((double)probe.M1_A == 57);
+        CHECK((double)probe.M2_A == 58);
+        CHECK((double)probe.M1_B == 59);
+        CHECK((double)probe.M2_B == 60);
+        CHECK((double)probe.N1_A == 61);
+        CHECK((double)probe.N2_A == 62);
+        CHECK((double)probe.N1_B == 63);
+        CHECK((double)probe.N2_B == 64);
     }
+}
 
-    SECTION("Free Field Format 3") {
+TEST_CASE("BDF PBEAM definitions (Free Field Format 3).", "[bdf_pbeam]") {
+
+    SECTION("check values") {
 
         std::list<std::string> data({
                 "PBEAM,1,2,3.,4.,5.,6.,7.,8.,9.,10.,11.,12.,13.,14.,15.,16.,"
@@ -175,15 +186,15 @@ TEST_CASE("BDF PBEAM definitions.", "[bdf_PBEAM]") {
         std::list<std::string> lines;
         __base::card::card_split(data, lines);
 
-        CAPTURE( data.front() );
-
-        REQUIRE(lines.size() == 66);
-
-        REQUIRE_THROWS(pbeam(lines));
-
+        CAPTURE(data.front());
+        CHECK(lines.size() == 66);
+        CHECK_THROWS(pbeam(lines));
     }
+}
 
-    SECTION("Tapered Beam") {
+TEST_CASE("BDF PBEAM definitions (Tapered Beam).", "[bdf_pbeam]") {
+
+    SECTION("check values") {
 
         std::list<std::string> data({
                 // 34567A1234567B1234567C1234567D1234567E1234567F1234567G1234567H1234567I
@@ -199,44 +210,44 @@ TEST_CASE("BDF PBEAM definitions.", "[bdf_PBEAM]") {
 
         CAPTURE( data.front() );
 
-        REQUIRE((long)probe.PID == 39);
-        REQUIRE((long)probe.MID == 6);
-        REQUIRE(probe.A == std::vector<double>({2.9, 5.3}));
-        REQUIRE(probe.I1 == std::vector<double>({3.5, 56.2}));
-        REQUIRE(probe.I2 == std::vector<double>({5.97, 78.6}));
-        REQUIRE(probe.I12 == std::vector<double>({0., 0.}));
-        REQUIRE(probe.J == std::vector<double>({0., 0.}));
-        REQUIRE(probe.NSM == std::vector<double>({0., 0.}));
-        REQUIRE(probe.C1 == std::vector<double>({0., 0.}));
-        REQUIRE(probe.C2 == std::vector<double>({0., 0.}));
-        REQUIRE(probe.D1 == std::vector<double>({2., 2.5}));
-        REQUIRE(probe.D2 == std::vector<double>({-4., -5.}));
-        REQUIRE(probe.E1 == std::vector<double>({0., 0.}));
-        REQUIRE(probe.E2 == std::vector<double>({0., 0.}));
-        REQUIRE(probe.F1 == std::vector<double>({0., 0.}));
-        REQUIRE(probe.F2 == std::vector<double>({0., 0.}));
+        CHECK((long)probe.PID == 39);
+        CHECK((long)probe.MID == 6);
+        CHECK(probe.A == std::vector<double>({2.9, 5.3}));
+        CHECK(probe.I1 == std::vector<double>({3.5, 56.2}));
+        CHECK(probe.I2 == std::vector<double>({5.97, 78.6}));
+        CHECK(probe.I12 == std::vector<double>({0., 0.}));
+        CHECK(probe.J == std::vector<double>({0., 0.}));
+        CHECK(probe.NSM == std::vector<double>({0., 0.}));
+        CHECK(probe.C1 == std::vector<double>({0., 0.}));
+        CHECK(probe.C2 == std::vector<double>({0., 0.}));
+        CHECK(probe.D1 == std::vector<double>({2., 2.5}));
+        CHECK(probe.D2 == std::vector<double>({-4., -5.}));
+        CHECK(probe.E1 == std::vector<double>({0., 0.}));
+        CHECK(probe.E2 == std::vector<double>({0., 0.}));
+        CHECK(probe.F1 == std::vector<double>({0., 0.}));
+        CHECK(probe.F2 == std::vector<double>({0., 0.}));
         std::vector<dnvgl::extfem::bdf::types::entry_value<std::string> > ref(1);
         dnvgl::extfem::bdf::types::entry_type<std::string> tmp("tmp");
         tmp.set_value(ref.front(), "YES");
-        REQUIRE(probe.SO == ref);
-        REQUIRE(probe.X_XB == std::vector<double>({1.}));
+        CHECK(probe.SO == ref);
+        CHECK(probe.X_XB == std::vector<double>({1.}));
 
-        REQUIRE((double)probe.K1 == 1.);
-        REQUIRE((double)probe.K2 == 1.);
-        REQUIRE((double)probe.S1 == 1.1);
-        REQUIRE((double)probe.S2 == 0.);
-        REQUIRE((double)probe.NSI_A == 2.1);
-        REQUIRE_FALSE(probe.NSI_B);
-        REQUIRE((double)probe.CW_A == 0.21);
-        REQUIRE_FALSE(probe.CW_B);
-        REQUIRE((double)probe.M1_A == 0.);
-        REQUIRE((double)probe.M2_A == 0.);
-        REQUIRE_FALSE(probe.M1_B);
-        REQUIRE_FALSE(probe.M2_B);
-        REQUIRE((double)probe.N1_A == 0.5);
-        REQUIRE((double)probe.N2_A == 0.);
-        REQUIRE((double)probe.N1_B == 0.0);
-        REQUIRE_FALSE(probe.N2_B);
+        CHECK((double)probe.K1 == 1.);
+        CHECK((double)probe.K2 == 1.);
+        CHECK((double)probe.S1 == 1.1);
+        CHECK((double)probe.S2 == 0.);
+        CHECK((double)probe.NSI_A == 2.1);
+        CHECK_FALSE(probe.NSI_B);
+        CHECK((double)probe.CW_A == 0.21);
+        CHECK_FALSE(probe.CW_B);
+        CHECK((double)probe.M1_A == 0.);
+        CHECK((double)probe.M2_A == 0.);
+        CHECK_FALSE(probe.M1_B);
+        CHECK_FALSE(probe.M2_B);
+        CHECK((double)probe.N1_A == 0.5);
+        CHECK((double)probe.N2_A == 0.);
+        CHECK((double)probe.N1_B == 0.0);
+        CHECK_FALSE(probe.N2_B);
     }
 }
 
@@ -245,5 +256,7 @@ TEST_CASE("BDF PBEAM definitions.", "[bdf_PBEAM]") {
 // coding: utf-8
 // c-file-style: "dnvgl"
 // indent-tabs-mode: nil
-// compile-command: "make -C ../cbuild -j8&&make -C ../cbuild test"
+// compile-command: "make -C ../cbuild -j7 &&
+//   (make -C ../cbuild test ;
+//    ../cbuild/tests/test_bdf_cards --use-colour no)"
 // End:
