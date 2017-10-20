1b800cee9ad081c2f8b8426b42a2a371836250a9
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2017-10-20 14:18:06 +0000

    Summary: Do not jump over variable declarations.
    git-svn-id: https://svnsrv.germanlloyd.org/svn/ExtFEMIO/trunk@760 389f8db8-dd28-11e4-b689-8b400cb78fb4

--- src/bdf/bdf_cards_pbeaml.cpp
+++ src/bdf/bdf_cards_pbeaml.cpp
@@ -138,6 +138,7 @@ pbeaml::pbeaml(long const *PID, long const *MID,
 void pbeaml::read(list<std::string> const & inp) {
 
     size_t i, j{0};
+    size_t dim_num;
 
     auto pos = inp.begin();
 
@@ -155,7 +156,7 @@ void pbeaml::read(list<std::string> const & inp) {
     if (pos == inp.end()) goto invalid;
     form_TYPE.set_value(TYPE, *(pos++));
     if (pos == inp.end()) goto invalid;
-    size_t const dim_num = this->l_geom::get_dim(TYPE.value);
+    dim_num = this->l_geom::get_dim(TYPE.value);
     if (dim_num < 1)
         throw errors::parse_error(
             "PBEAML", "Unknown beam type " + TYPE.value + ".");
